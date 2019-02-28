a3a010f62dedf8e58b60172c1857f419598095d1
Surupa Chatterjee <chatterjee.surupa63@gmail.com>
2019-02-28 13:27:50 -0600

    initial commit

---
+++ src/app/chat/chat.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment';
+import { ApiAiClient } from 'api-ai-javascript/es6/ApiAiClient';
+import {Observable} from 'rxjs/Observable';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+
+
+export class  Message {
+  constructor(public  content: {}, public sentBy: string, public contentType: string) {
+  }
+}
+
+@Injectable()
+export class ChatService {
+
+  readonly  token = environment.dialogflow.angularBot;
+  readonly client = new ApiAiClient({accessToken: this.token});
+  conversation = new BehaviorSubject<Message[]>([]);
+
+
+  constructor() { }
+
+  update(msg: Message) {
+    this.conversation.next([msg]);
+  }
+
+  converse(msg: string) {
+    const userMessage = new Message(msg, 'user','Text');
+    this.update(userMessage);
+    let speech;
+    let contentType;
+    return this.client.textRequest(msg)
+      .then(res => {
+        // console.log(res);
+        // console.log(res.result.fulfillment);
+
+        if (res.result.fulfillment.speech !== '') {
+          speech = res.result.fulfillment.speech;
+          contentType = 'Text';
+        } else {
+          speech = res.result.fulfillment.messages[0].payload.wells;
+          contentType = 'wellsArray';
+          console.log(speech);
+        }
+        // console.log(speech[0].payload);
+        // const wellarray = speech[0].payload.wells;
+        // console.log(wellarray);
+
+        const botMessage = new Message(speech, 'bot', contentType);
+        this.update(botMessage);
+      });
+  }
+  talk() {
+    this.client.textRequest('Who are you?')
+      .then(res => console.log(res));
+
+}
+
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  dialogflow: {
+    angularBot: 'a51a147fbd144d2595d21b0a73517081'
+  }
 };
 
 /*
